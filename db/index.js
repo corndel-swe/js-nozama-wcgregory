bc635db9313536a7958b5402c7c3ddd9e4c14145
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-16 15:52:11 +0000

    create, delete and login methods added to User model

--- db/index.js
+++ db/index.js
@@ -9,7 +9,7 @@ const dbPath = new URL('./db.sqlite', import.meta.url)
 
 const db = knex({
   client: 'sqlite3',
-	connection: {filename: fileURLToPath(dbPath)},
+  connection: {filename: fileURLToPath(dbPath)},
 	useNullAsDefault: true
 })
 
--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
+import { boolean } from 'zod'
 import db from '../db/index.js'
 
 class User {
   static async findAll() {
     const query = `
       select id, username, firstName, lastName, email, avatar 
-      from users
+      from users;
     `
     const results = await db.raw(query)
     return results
@@ -16,11 +17,62 @@ class User {
      */
     const query = `
       SELECT id, username, firstName, lastName, email, avatar
-      FROM users WHERE id = ?
+      FROM users WHERE id = ?;
     `
     const results = await db.raw(query, [id])
     return results[0]
   }
+
+  static async findByUserName(username) {
+    const query = `
+      SELECT id, username, firstName, lastName, email, avatar
+      FROM users WHERE username = ?;
+    `
+    const results = await db.raw(query, [username])
+    return results[0]
+  }
+
+  static async create(username, firstName, lastName, email, avatar, password) {
+    const query = `
+      INSERT INTO users (username, firstName, lastName, email, avatar, password)
+      VALUES (?, ?, ?, ?, ?, ?) RETURNING *;
+    `
+    const addedUser = await db.raw(query, [username, firstName, lastName, email, avatar, password])
+    delete addedUser[0].password
+    return addedUser[0]
+  }
+
+  static async delete(id) {
+    const query = `
+      DELETE FROM users WHERE users.id = ? RETURNING *;
+    `
+    const idExists = await User.findById(id)
+    if (!await idExists) return {"result": false, "reason": `User id ${id} doesn't exist!`};
+    else {const deletedUser = await db.raw(query, [id])
+      delete deletedUser[0].password
+      return {"result": true, "deleted": deletedUser[0]}
+    }
+  }
+
+  static async login(username, password) {
+    const query = `
+        SELECT password
+        FROM users WHERE username = ?;
+      `
+    const userSearch = await User.findByUserName(username)
+    if (userSearch) {
+      const userPassword = await db.raw(query, [username])
+      if (userPassword[0].password === password) return userSearch;
+    }
+    return {"result": false, "reason": `Username or password is invalid!`}
+  }
 }
 
 export default User
+
+//console.log(await User.create('fab5freddy', 'Freddy', 'Flintstone', 'fab5freddy@gmail.com', 'None.img', '12345LetMeIn'))
+//console.log(await User.findById(202))
+//console.log(await User.delete(203))
+//console.log(await User.findById(200))
+//console.log(await User.login('fab5freddy', '12345LetMe'))
+//db.destroy()
